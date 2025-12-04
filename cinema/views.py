ccb16445d6d5a4f58d6451146f7ec425e6a236e2
Alexandr <oleksandrglushen2005@gmail.com>
2025-12-04 14:56:50 +0200

    fixed code 2

--- cinema/views.py
+++ cinema/views.py
@@ -14,10 +14,12 @@ from cinema.serializers import (GenreSerializer,
 
 
 class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
     def get_serializer_class(self) -> type[serializers.ModelSerializer]:
@@ -32,6 +34,7 @@ class MovieViewSet(viewsets.ModelViewSet):
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
+    queryset = MovieSession.objects.all()
     serializer_class = MovieSessionSerializer
 
     def get_serializer_class(self) -> type[serializers.ModelSerializer]:
@@ -46,8 +49,10 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
 
 
 class ActorViewSet(viewsets.ModelViewSet):
+    queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
 
 class CinemaHallViewSet(viewsets.ModelViewSet):
+    queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
